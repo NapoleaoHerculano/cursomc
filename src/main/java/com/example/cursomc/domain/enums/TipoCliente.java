83c7197fe9bf5bd74ba82ceaad1601c896800e4c
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2019-12-24 12:38:14 -0300

    Cliente, TipoCliente, Telefones e Endereços

---
+++ src/main/java/com/example/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,36 @@
+package com.example.cursomc.domain.enums;
+
+public enum TipoCliente {
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+	
+	private int codigo;
+	private String descricao;
+	
+	private TipoCliente(int codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static TipoCliente toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for(TipoCliente t: TipoCliente.values()) {
+			if (cod.equals(t.getCodigo())){
+				return t;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+}
